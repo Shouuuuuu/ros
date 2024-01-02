810602d639dec9acb313c59b646f36b38e958786
Shoma Takatori <shoma@hogehoge.com>
2024-01-02 19:11:30 +0900

    ros2

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,15 +2,17 @@ import rclpy
 from rclpy.node import Node
 from person_msgs.srv import Query
 
-def cb(request, response):
-    if request.name == "高鳥翔馬":
-        response.age = 19
-    else:
-        response.age = 255
-
-    return response
-
 rclpy.init()
 node = Node("talker")
-srv = node.create_service(Query, "query", cb)
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+
+def cd ():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
 rclpy.spin(node)
